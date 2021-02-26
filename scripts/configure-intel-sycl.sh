aa9537cfc5d8b880784d7c279931e9c3db81c197
Shreyas Ananthan <shreyas@umd.edu>
2021-02-26 14:00:25 -0700

    Add intel sycl/openmp build

---
+++ scripts/configure-intel-sycl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+exw_virtuals_configure ()
+{
+    local extra_args="$@"
+
+    local cmake_cmd=(
+        cmake
+        -DCMAKE_CXX_COMPILER=$(which dpcpp)
+        -DCMAKE_C_COMPILER=$(which clang)
+        -DKokkos_ENABLE_SERIAL=ON
+        -DKokkos_ENABLE_OPENMP=OFF
+        -DKokkos_ENABLE_CUDA=OFF
+        -DKokkos_ENABLE_HIP=OFF
+        -DKokkos_ENABLE_OPENMPTARGET=OFF
+        -DKokkos_ENABLE_SYCL=ON
+        -DKokkos_ARCH_INTEL_GEN=ON
+        ${extra_args}
+        ${EXW_VIRTUALS_SOURCE_DIR:-..}
+    )
+
+    echo "${cmake_cmd[@]}" | tee cmake_output.log
+    eval "${cmake_cmd[@]}" |& tee -a cmake_output.log
+}
+
+exw_virtuals_configure "$@"
